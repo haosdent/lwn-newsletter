74ce686d580342d573c506872ae16a42eb52fe59
haosdent <haosdent@gmail.com>
2015-01-27 09:42:58 +0800

    Use config in main.

--- src/main.go
+++ src/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+    "os"
+    "log"
+    "fmt"
     "lwn"
+    "strconv"
+    "github.com/haosdent/commons-configuration"
 )
 
 func main() {
-    config, err := NewIniConfig()
-    receiver := config.Get("receiver")
-    password := config.Get("password")
-    server := config.Get("server")
-    port := config.Get("port")
+    if len(os.Args) != 2 {
+        fmt.Printf("[Usage]:\n\t %s [config file location]\n", os.Args[0])
+        os.Exit(-1)
+    }
+
+    path := os.Args[1]
+    var conf config.Configer = config.NewIniConfig(path)
+    receiver, err := conf.Get("receiver")
+    if err != nil {
+        log.Fatal(err)
+    }
+    password, err := conf.Get("password")
+    if err != nil {
+        log.Fatal(err)
+    }
+    server, err := conf.Get("server")
+    if err != nil {
+        log.Fatal(err)
+    }
+    sPort, err := conf.Get("port")
+    if err != nil {
+        log.Fatal(err)
+    }
+    port, err := strconv.Atoi(sPort)
+    if err != nil {
+        log.Fatal(err)
+    }
     content := lwn.GetLwnContent()
     lwn.SendEmail(content, receiver, password, server, port)
+    fmt.Printf("Send lwn weekly to %s success!\n", receiver)
 }
